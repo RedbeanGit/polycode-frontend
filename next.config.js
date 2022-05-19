2a36428ffe335904ec83b7bc59d57ef7fe081177
RedbeanGit <dubois.julien.mail@gmail.com>
2022-05-19 22:07:32 +0200

    feat: Add docker file
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  images: {
-    domains: ['cdn.pixabay.com'],
-  },
 }
 
 module.exports = nextConfig
